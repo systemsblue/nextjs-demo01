d5761c55d1954e2428208d56edd6d91e7615d94e
Kei <n@iiv.jp>
2023-11-20 15:33:26 +0900

    change config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+//const branchName = process.env.BRANCH_NAME ? "/" + process.env.BRANCH_NAME : "";
+
+const branchName = 'nextjs-demo01';
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: branchName,
+  basePath: branchName,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
